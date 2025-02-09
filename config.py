d14626888a86e162543ef928790f6970845859fb
MaksimKhmylov <maksimhmylov97@gmail.com>
2025-02-09 15:57:04 +0300

    Initinal

---
+++ config.py
@@ -0,0 +1,11 @@
+import enum
+
+API_ID = 22543588
+API_HASH = '7e20f617e664855078a41a98df729c1c'
+API_TOKEN = '7051266336:AAFsZr-ZuitAV2nvRSrJMCiYX7MtwS1KSGw'
+
+class UserStatus(enum.Enum):
+
+    new = 1
+    standby = 2
+    edit = 3
